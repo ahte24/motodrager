0b60e9aa511affa85ff7f2a263454c18d929c51b
Mohammed Ahteshamuz Zaman <ahteshamzaman2024@gmail.com>
2024-09-14 23:20:25 +0530

    initial commit

--- components/component/component.tsx
+++ components/component/component.tsx
@@ -11,8 +11,7 @@ export function Component() {
     <>
       <header className="px-4 lg:px-6  h-16 flex items-center border-b">
         <Link href="#" className="flex items-center justify-center" prefetch={false}>
-          <MountainIcon/>
-          <span className="sr-only">Lubricate Inc</span>
+          <span className="text-2xl font-semibold">MotoDrager</span>
         </Link>
         <nav className="ml-auto flex gap-4 sm:gap-6">
           <Link href="#" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
